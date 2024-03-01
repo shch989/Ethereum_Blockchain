fc25f806083334b31ded8a6f1f99538da08aef9e
shch989 <tankpop11@gmail.com>
2024-03-02 01:05:09 +0900

    0.4.17 버전 프로젝트 완료

---
+++ lottery-react/README.md
@@ -0,0 +1,8 @@
+## Create React App
+```
+npx create-react-app lottery-react
+```
+## Web3
+```
+npm install web3 --save
+```
---
+++ lottery-react/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import web3 from './web3'
+import lottery from './lottery'
+
+const App = () => {
+  const [manager, setManager] = useState('')
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState('')
+  const [value, setValue] = useState('')
+  const [message, setMessage] = useState('')
+
+  const fetchData = async () => {
+    const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+    setManager(manager)
+    setPlayers(players)
+    setBalance(balance)
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    const accounts = await web3.eth.getAccounts()
+
+    setMessage('Waiting on transaction success...')
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, 'ether'),
+    })
+
+    setMessage('You have been entered!')
+  }
+
+  const onClick = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    setMessage('Waiting on transaction success...')
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+
+    setMessage('A winner has been picked!')
+  }
+
+  fetchData()
+
+  return (
+    <div onSubmit={onSubmit}>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {manager}.<br />
+        There are currently {players.length} people entered,
+        <br />
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+      <hr />
+      <form>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onClick}>Pick a winner!</button>
+      <hr />
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default App
